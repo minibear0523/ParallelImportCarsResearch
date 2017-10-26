d352df231430c847a8e3023058a32d605d2771a6
minibear <lei.zhang.bupt08@gmail.com>
2017-10-26 19:10:05 +0800

    MOD:增加参考文献

--- source/autohome_cars.py
+++ source/autohome_cars.py
@@ -11,13 +11,18 @@ from gevent.pool import Pool
 from gevent.queue import Queue
 from urllib.parse import urljoin
 from pymongo import MongoClient
+import time
+import hashlib
+
+ORDER_NO = 'ZF201710145848mpry3V'
+SECRET = '1d3a17f969724c978a36115ed39bce06'
 
 client = MongoClient()
 db = client['Market_Research']
 collection = db['autohome']
 
 # 设置Pool和Queue
-pool = Pool(size=10)
+pool = Pool(size=5)
 list_queue = Queue()
 detail_queue = Queue()
 item_queue = Queue()
@@ -28,10 +33,10 @@ START_URL = "/list/0-999999-0-0-0-0-0-0-0-1.html?factoryId=0&minPrice=-1&maxPric
 BASE_URL = "https://mall.autohome.com.cn"
 
 headers = {
-    'Cookie': 'sessionid=7998F0BB-1D51-EC57-FA22-6D0D9D1CCFE6%7C%7C2016-01-10+10%3A49%3A58.174%7C%7Cwww.google.com; fvlid=1467993576377O8YaFrge; WarningClose=1; UM_distinctid=15e3cb23a862c-09011349eddaac-143a6d54-1aeaa0-15e3cb23a87bf5; historybbsName4=c-588%7C%E5%A5%94%E9%A9%B0C%E7%BA%A7; cityId=110100; cookieCityId=120100; ahpau=1; nologinid-voteid=636185841200229140; PraiseKey=4032c70a-54db-4192-867b-eeedec08397e; __utma=1.516336464.1452394200.1468077953.1506872540.15; __utmc=1; __utmz=1.1506872540.15.1.utmcsr=autohome.com.cn|utmccn=(referral)|utmcmd=referral|utmcct=/3294/; mallsfvi=1506902163496A8cMwZ8i%7Cwww.autohome.com.cn%7C2018267; product_compare=27363%2C27364; ahsids=703_162_415_264_95_4175; sessionip=111.160.31.234; wwwjbtab=0%2C0; sessionuid=7998F0BB-1D51-EC57-FA22-6D0D9D1CCFE6%7C%7C2016-01-10+10%3A49%3A58.174%7C%7Cwww.google.com; mpvareaid=2018223; mallslvi=2018223%7C0%7C1508909609247Xqluq0fw; hasGetRedPackage=0; mallCityId=999999; ahpvno=41; ref=www.google.com%7C0%7C0%7C0%7C2017-10-25+13%3A35%3A48.316%7C2017-10-25+11%3A46%3A38.483; sessionvid=FF4C43FD-71D0-419D-8083-C80AB1C41DEC; area=120199; ahrlid=1508909744864n4NNbE6K-1508909755006',
+    'Cookie': 'UM_distinctid=15f50c9c9222c2-02ae0f49d2afbb-31657c00-384000-15f50c9c923681; fvlid=1508888921932UuGMXqGaBI; sessionip=117.15.125.251; sessionid=C30F3DFE-3553-4381-AF57-BBF39F1497C3%7C%7C2017-10-25+07%3A48%3A47.666%7C%7C0; ahpau=1; ahsids=4348; sessionuid=C30F3DFE-3553-4381-AF57-BBF39F1497C3%7C%7C2017-10-25+07%3A48%3A47.666%7C%7C0; mpvareaid=2023627; mallsfvi=1508929472012tWqpLlMq%7Cwww.autohome.com.cn%7C103414; mallslvi=103414%7Cwww.autohome.com.cn%7C1508929472012tWqpLlMq; hasGetRedPackage=0; mallCityId=999999; ahpvno=29; ref=0%7C0%7C0%7C0%7C2017-10-25+19%3A44%3A54.326%7C2017-10-25+07%3A48%3A47.666; sessionvid=6159878B-1D90-4E27-A6BE-AE4A5144CAEB; area=120199',
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
     'Host': 'mall.autohome.com.cn',
-    'Referer': 'https://mall.autohome.com.cn/list/0-120100-0-0-0-0-0-0-0-1.html?factoryId=0&minPrice=-1&maxPrice=-1&specId=0&stageTag=0&importTag=1&double11Tag=0&prefix=&dataSource=&minDownPay=&maxDownPay=&businessType=&providerId=',
+    'Referer': 'https://mall.autohome.com.cn/list/0-999999-0-0-0-0-0-0-0-1.html?factoryId=0&minPrice=-1&maxPrice=-1&stageTag=0&importTag=1&double11Tag=0&dataSource=&eventId=&eventProcessId=&providerId=&itemIds=&eventProcessIds=&providerIds=&dataSources=&specId=0&minDownPay=&maxDownPay=&businessType=',
     'X-Requested-With': 'XMLHttpRequest',
     'Accept': '*/*',
     'Accept-Encoding': 'gzip,deflate,br',
@@ -39,8 +44,23 @@ headers = {
     'Upgrade-Insecure-Requests': '1'
 }
 
+proxies = {
+    'http': 'http://forward.xdaili.cn:80',
+    'https': 'https://forward.xdaili.cn:80'
+}
+
+def form_sign():
+    timestamp = int(time.time())
+    plain_text = 'orderno=%s,secret=%s,timestamp=%s' % (ORDER_NO, SECRET, timestamp)
+    sign = hashlib.md5(plain_text.encode('utf-8')).hexdigest().upper()
+    auth = 'sign=%s&orderno=%s&timestamp=%s' % (sign, ORDER_NO, timestamp)
+    return auth
+
+
 def fetch(url):
-    req = requests.get(urljoin(BASE_URL, url), headers=headers, verify=False)
+    h = headers.copy()
+    h['Proxy-Authorization'] = form_sign()
+    req = requests.get(urljoin(BASE_URL, url), headers=headers, timeout=10)
     if req.status_code != 200:
         print('%s:请求失败--%s' % (url, req.status_code))
         return None
@@ -54,12 +74,14 @@ def crawl():
         tree = etree.HTML(html)
         for car in tree.xpath('//li[@class="carbox"]'):
             spec_id = ''.join(car.xpath('./a/span[@class="carbox-compare"]/@link')).strip()
-            price = ''.join(car.xpath('./a/div[@class="carbox-info"]/span/text()')).strip()
             item_id = car.xpath('./@data-itemid')[0].strip()
-            detail_queue.put({'spec_id': spec_id, 'price': price, 'item_id': item_id})
+            detail_queue.put({'spec_id': spec_id, 'item_id': item_id})
 
-        for page in tree.xpath('//span[@class="pager-pageindex"]/a/@href'):
-            list_queue.put(page)
+        next_page = tree.xpath('//a[@class="pager-next"]/@href')
+        if len(next_page) > 0:
+            next_page = next_page[0].strip()
+            list_queue.put(next_page)
+            time.sleep(1)
 
 def list_loop():
     while True:
@@ -75,11 +97,13 @@ def crawl_list_page(url):
         tree = etree.HTML(html)
         for car in tree.xpath('//li[@class="carbox"]'):
             spec_id = ''.join(car.xpath('./a/span[@class="carbox-compare"]/@link')).strip()
-            price = ''.join(car.xpath('./a/div[@class="carbox-info"]/span/text()')).strip()
             item_id = car.xpath('./@data-itemid')[0].strip()
-            detail_queue.put({'spec_id': spec_id, 'price': price, 'item_id': item_id})
-        for page in tree.xpath('//span[@class="pager-pageindex"]/a/@href'):
-            list_queue.put(page)
+            detail_queue.put({'spec_id': spec_id, 'item_id': item_id})
+        next_page = tree.xpath('//a[@class="pager-next"]/@href')
+        if len(next_page) > 0:
+            next_page = next_page[0].strip()
+            list_queue.put(next_page)
+            time.sleep(1)
 
 def detail_loop():
     while True:
@@ -95,13 +119,25 @@ def crawl_detail_page(car_info):
     RET: {'returncode', 'message', 'result':{'specid', 'paramtypeitems': [{id, name, pordercls, paramitems}]}}
     车型名称, 厂商指导价(元), 厂商, 级别, 发动机, 价格
     """
+    url = 'https://mall.autohome.com.cn/detail/getItemsDetail.html?itemIds=%s' % car_info['item_id']
+    req = requests.get(url, headers=headers, timeout=10)
+    if req.status_code != 200:
+        print('%s:请求失败!' % url)
+    results = req.json()['data']
+    car_info['price'] = results[0]['price']
+    car_info['brand'] = results[0]['brandName']
+    car_info['official_price'] = results[0]['productPrice']
+    time.sleep(1)
+
     url = urljoin(BASE_URL, '/http/data.html')
     params = {
         'data[_host]': '//api.admin.mall.m6.autohome.com.cn/tradespec/getParameters.jtml',
         'data[_appid]': 'mall',
         'data[specid]': car_info['spec_id']
     }
-    req = requests.get(url, params=params, headers=headers)
+    h = headers.copy()
+    h['Proxy-Authorization'] = form_sign()
+    req = requests.get(url, params=params, headers=headers, timeout=10)
     if req.status_code != 200:
         print('%s:请求失败!' % car_info['spec_id'])
         return
@@ -109,16 +145,13 @@ def crawl_detail_page(car_info):
     for param in result:
         if param['name'] == '车型名称':
             car_info['name'] = param['value']
-        elif param['name'] == '厂商指导价(元)':
-            car_info['official_price'] = param['value']
-        elif param['name'] == '厂商':
-            car_info['brand'] = param['value']
         elif param['name'] == '级别':
             car_info['level'] = param['value']
         elif param['name'] == '发动机':
             car_info['engine'] = param['value']
 
     item_queue.put(car_info)
+    time.sleep(2)
 
 def db_loop():
     while True:
